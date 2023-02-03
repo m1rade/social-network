daaccfbfe208169b1c7bb8d75ff32dd69198c5bf
m1rade <forx98@gmail.com>
2023-02-04 01:02:20 +0300

    Changed typification

--- src/containers/ProfileInfoContainer.tsx
+++ src/containers/ProfileInfoContainer.tsx
@@ -1,19 +1,21 @@
 import React from "react";
-import { connect } from "react-redux";
-import { ProfileInfo } from "../components/Profile/ProfileInfo/ProfileInfo";
-import { AppStateType } from "../redux/store";
+import {connect} from "react-redux";
+import {ProfileInfo} from "../components/Profile/ProfileInfo/ProfileInfo";
+import {AppStateType} from "../redux/store";
 
-type ProfileInfoContainerPropsType = {
-    name: string;
-    description: string;
-    avatar: string;
-};
+type mapStateToPropsType = {
+    name: string,
+    description: string,
+    avatar: string
+}
+
+export type ProfileInfoContainerPropsType = mapStateToPropsType;
 
 export const ProfileInfoContainer: React.FC<ProfileInfoContainerPropsType> = (props) => {
-    return <ProfileInfo name={props.name} description={props.description} avatar={props.avatar} />
+    return <ProfileInfo name={props.name} description={props.description} avatar={props.avatar}/>
 };
 
-const mapStateToProps = (state: AppStateType): ProfileInfoContainerPropsType => ({
+const mapStateToProps = (state: AppStateType): mapStateToPropsType => ({
     name: state.profile.name,
     description: state.profile.description,
     avatar: state.profile.avatar,
