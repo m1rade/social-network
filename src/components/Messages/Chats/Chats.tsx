4c8ce0e1cb247652f59986e6375c46cb13c91676
m1rade <forx98@gmail.com>
2023-02-03 18:41:20 +0300

    Add Chats component

---
+++ src/components/Messages/Chats/Chats.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import style from "./Dialogs.module.css";
+import {NavLink} from "react-router-dom";
+
+type ChatsPropsType = {
+    userID: number,
+    userName: string
+}
+
+export const Chats = (props: ChatsPropsType) => {
+    return (
+        <div className={style.dialogs}>
+            <div className={style.dialogItem}>
+                <NavLink activeClassName={style.active} to={`/dialog/${props.userID}`}>{props.userName}</NavLink>
+            </div>
+        </div>
+    );
+};
