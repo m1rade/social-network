030b788bb71eba65f81b7e4806a61192d931ccdc
m1rade <forx98@gmail.com>
2023-02-03 19:17:06 +0300

    Optimized imports

--- src/components/Messages/Messages.tsx
+++ src/components/Messages/Messages.tsx
@@ -1,16 +1,16 @@
 import React from "react";
-import DialogsContainer from "../../containers/DialogsContainer";
-import { MessageAreaContainer } from "../../containers/MessageAreaContainer";
+import ChatsContainer from "../../containers/ChatsContainer";
+import { DialogContainer } from "../../containers/DialogContainer";
 import style from "./Messages.module.css";
 
 export const Messages = () => {
     return (
         <div className={style.messages}>
             <div className={style.dialogs_area}>
-                <DialogsContainer />
+                <ChatsContainer />
             </div>
             <div className={style.messages_area}>
-                <MessageAreaContainer />
+                <DialogContainer />
             </div>
         </div>
     );
