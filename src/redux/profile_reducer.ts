1a4d86f06b105c7d637c80d33904a57a8275fd9e
m1rade <forx98@gmail.com>
2023-02-10 21:15:04 +0300

    Refactored for ajax request

--- src/redux/profile_reducer.ts
+++ src/redux/profile_reducer.ts
@@ -1,37 +1,43 @@
-export type ProfileType = {
-    id: number;
-    name: string;
-    description: string;
-    avatar: string;
-    posts: PostType[];
-    newPostMessage: string;
-};
-
-export type PostType = {
-    id: number;
-    message: string;
-};
-
-export type ProfileActionType = updatePostMessageACType | addPostMessageACType;
+import { ToggleIsFetchingType, UserPhotoType } from "./search_reducer";
 
 const UPDATE_NEW_POST_MESSAGE = "UPDATE-NEW-POST-MESSAGE";
 const ADD_POST = "ADD-POST";
+const SET_USER_INFO = "SET_USER_INFO";
+const TOGGLE_IS_FETCHING = "TOGGLE_IS_FETCHING";
 
-const initState: ProfileType = {
-    id: 1,
-    name: "Cooool cat",
-    description: "Cooool cat's description here",
-    avatar: "https://i.pinimg.com/originals/ae/24/87/ae24874dd301843548c034a3d2973658.png",
+const initState: ProfileDomainType = {
+    aboutMe: "Cooool cat's description here",
+    contacts: {
+        facebook: "facebook.com",
+        website: null,
+        vk: "vk.com/cat",
+        twitter: "https://twitter.com/",
+        instagram: "instagra.com/",
+        youtube: null,
+        github: "github.com/",
+        mainLink: null,
+    },
+    lookingForAJob: false,
+    lookingForAJobDescription: "gfgf",
+    fullName: "Cooool cat",
+    userId: 1,
+    photos: {
+        small: "https://i.pinimg.com/originals/ae/24/87/ae24874dd301843548c034a3d2973658.png",
+        large: "",
+    },
     posts: [
         { id: 1, message: "How are you today?" },
         { id: 2, message: "Hello world!" },
         { id: 3, message: "I ate" },
     ],
     newPostMessage: "",
+    isFetching: false,
 };
 
-const profile_reducer = (state: ProfileType = initState, action: ProfileActionType): ProfileType => {
+const profile_reducer = (state: ProfileDomainType = initState, action: ProfileActionType): ProfileDomainType => {
     switch (action.type) {
+        case SET_USER_INFO:
+            return { ...state, ...action.userInfo };
         case UPDATE_NEW_POST_MESSAGE:
             return { ...state, newPostMessage: action.newPostMessage };
         case ADD_POST:
@@ -44,22 +50,66 @@ const profile_reducer = (state: ProfileType = initState, action: ProfileActionTy
                 posts: [newPostObj, ...state.posts],
                 newPostMessage: "",
             };
+        case TOGGLE_IS_FETCHING:
+            return { ...state, isFetching: action.value };
         default:
             return state;
     }
 };
+export default profile_reducer;
 
-type updatePostMessageACType = ReturnType<typeof updatePostMessageAC>;
-export const updatePostMessageAC = (newPostMessage: string) =>
+//actions
+export const updatePostMessage = (newPostMessage: string) =>
     ({
         type: UPDATE_NEW_POST_MESSAGE,
         newPostMessage,
     } as const);
 
-type addPostMessageACType = ReturnType<typeof addPostMessageAC>;
-export const addPostMessageAC = () =>
+export const addPostMessage = () =>
     ({
         type: ADD_POST,
     } as const);
 
-export default profile_reducer;
+export const setUserInfo = (userInfo: ProfileDomainType) =>
+    ({
+        type: SET_USER_INFO,
+        userInfo,
+    } as const);
+
+//types
+type ProfileResponseType = {
+    aboutMe: string;
+    contacts: ContactsType;
+    lookingForAJob: boolean;
+    lookingForAJobDescription: string;
+    fullName: string;
+    userId: number;
+    photos: UserPhotoType;
+};
+
+type ContactsType = {
+    facebook: string | null;
+    website: string | null;
+    vk: string | null;
+    twitter: string | null;
+    instagram: string | null;
+    youtube: string | null;
+    github: string | null;
+    mainLink: string | null;
+};
+
+export type PostType = {
+    id: number;
+    message: string;
+};
+
+export type ProfileDomainType = ProfileResponseType & {
+    posts: PostType[];
+    newPostMessage: string;
+    isFetching: boolean;
+};
+export type ProfileActionType =
+    | ReturnType<typeof updatePostMessage>
+    | ReturnType<typeof addPostMessage>
+    | ReturnType<typeof setUserInfo>
+    | ToggleIsFetchingType;
