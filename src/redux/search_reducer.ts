c6697c31e8952b699ce6620027df6bea7a8f1dc7
m1rade <forx98@gmail.com>
2023-02-10 20:28:27 +0300

    Refactored

--- src/redux/search_reducer.ts
+++ src/redux/search_reducer.ts
@@ -1,3 +1,10 @@
+const SET_USERS = "SET_USERS"
+const SET_TOTAL_COUNT = "SET_TOTAL_COUNT"
+const FOLLOW_UNFOLLOW_USER = "FOLLOW_UNFOLLOW_USER"
+const SET_CURRENT_PAGE = "SET_CURRENT_PAGE";
+const SET_TOTAL_PAGES = "SET_TOTAL_PAGES";
+const TOGGLE_IS_FETCHING = "TOGGLE_IS_FETCHING"
+
 const initialState: SearchPageDomainType = {
     items: [],
     totalCount: 0,
@@ -12,22 +19,20 @@ const search_reducer = (
     action: SearchActionType
 ): SearchPageDomainType => {
     switch (action.type) {
-        case "SET_USERS":
+        case SET_USERS:
             return { ...state, items: [...action.items] };
-        case "SET_TOTAL_COUNT":
+        case SET_TOTAL_COUNT:
             return { ...state, totalCount: action.totalCount };
-        case "FOLLOW/UNFOLLOW_USER":
+        case FOLLOW_UNFOLLOW_USER:
             return {
                 ...state,
-                items: state.items.map((i) =>
-                    i.id === action.id ? { ...i, followed: action.value } : i
-                ),
+                items: state.items.map((i) => (i.id === action.id ? { ...i, followed: action.value } : i)),
             };
-        case "SET_CURRENT_PAGE":
+        case SET_CURRENT_PAGE:
             return { ...state, curPage: action.page };
-        case "SET_TOTAL_PAGES":
+        case SET_TOTAL_PAGES:
             return { ...state, itemsPerPage: action.totalPages };
-        case "TOGGLE_IS_FETCHING":
+        case TOGGLE_IS_FETCHING:
             return { ...state, isFetching: action.value };
         default:
             return state;
@@ -39,36 +44,36 @@ export default search_reducer;
 //actions
 export const follow_unfollowHandler = (id: number, value: boolean) =>
     ({
-        type: "FOLLOW/UNFOLLOW_USER",
+        type: FOLLOW_UNFOLLOW_USER,
         id,
         value,
     } as const);
 
 export const setUsers = (items: UserType[]) =>
     ({
-        type: "SET_USERS",
+        type: SET_USERS,
         items,
     } as const);
 export const setCurrentPage = (page: number) =>
     ({
-        type: "SET_CURRENT_PAGE",
+        type: SET_CURRENT_PAGE,
         page,
     } as const);
 export const setTotalPages = (totalPages: number) =>
     ({
-        type: "SET_TOTAL_PAGES",
+        type: SET_TOTAL_PAGES,
         totalPages,
     } as const);
 
 export const setTotalCount = (totalCount: number) =>
     ({
-        type: "SET_TOTAL_COUNT",
+        type: SET_TOTAL_COUNT,
         totalCount,
     } as const);
 
 export const toggleIsFetching = (value: boolean) =>
     ({
-        type: "TOGGLE_IS_FETCHING",
+        type: TOGGLE_IS_FETCHING,
         value,
     } as const);
 
@@ -99,10 +104,11 @@ type SearchPageDomainType = SearchResponseType & {
     isFetching: boolean;
 };
 
+export type ToggleIsFetchingType = ReturnType<typeof toggleIsFetching>
 export type SearchActionType =
     | ReturnType<typeof follow_unfollowHandler>
     | ReturnType<typeof setUsers>
     | ReturnType<typeof setCurrentPage>
     | ReturnType<typeof setTotalPages>
     | ReturnType<typeof setTotalCount>
-    | ReturnType<typeof toggleIsFetching>;
+    | ToggleIsFetchingType;
